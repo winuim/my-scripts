b68c25d423994b39cbe727ec5fa93d0923c4912c
Yohei Uema <winuim@gmail.com>
2019-05-28 19:42:48 +0900

    ftp upload usging curl

---
+++ curl-ftp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -eu
+
+[[ -v DEBUG ]] && ${DEBUG} && set -x
+
+FILE_LIST=$(find $1 -type f -not -name '.DS_Store')
+DELETE_PATH=${1%/*}
+
+for file_path in $FILE_LIST;
+do
+  ftp_path="$2${file_path/$DELETE_PATH/}";
+  curl -T $file_path --ftp-create-dirs $ftp_path;
+done
+
